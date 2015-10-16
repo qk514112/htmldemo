d62a38c0d20cce07076584a945801ab798c3c786
dj0708 <dj0708@dj0708deMacBook-Pro.local>
2015-10-16 13:36:32 +0800

    initial commit

---
+++ cutomers_.php
@@ -0,0 +1,21 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$conn = new mysqli("localhost", "root", "123456", "northwind");
+
+$result = $conn->query("SELECT name, city, country FROM customers");
+
+$outp = "";
+while ($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+  if ($outp != "") {$outp .= ",";} 
+  $outp .= '{"Name":"' . $rs["name"] . '",';
+  $outp .= '"City":"' . $rs["City"] . '",';
+  $outp .= '"Country":"' . $rs["Country"] . '"}';
+}
+$outp = '{"records":[' . $outp . ']}';
+
+$conn->close();
+
+echo($outp);
+?>
